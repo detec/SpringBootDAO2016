7e639339018844df4fe892337371aba2eb11335e
duplyk.a <dlit@yandex.ru>
2016-11-10 17:01:19 +0200

    Added SpringFox swagger.

--- src/main/java/sample/config/WebAppConfig.java
+++ src/main/java/sample/config/WebAppConfig.java
@@ -40,6 +40,11 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+
+		// adding swagger-ui resources.
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 	@Override
--- src/main/java/sample/controller/SampleRest.java
+++ src/main/java/sample/controller/SampleRest.java
@@ -7,6 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 /**
  * Sample controller class to check application is alive.
  *
@@ -17,24 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleRest {
 
-	/**
-	 * Live test endpoint
-	 *
-	 * @return true as JSON
-	 */
+	@ApiOperation(value = "testing liveliness of service", nickname = "test")
 	@GetMapping("test")
-	public ResponseEntity<Boolean> testController() {
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = Boolean.class) })
+
+	ResponseEntity<Boolean> testController() {
 		return new ResponseEntity<>(Boolean.TRUE, HttpStatus.OK);
 	}
 
-	/**
-	 * Endpoint to test LocalDateTime output, taht should be formatted as ISO
-	 * date.
-	 *
-	 * @return wrapped entity
-	 */
+	@ApiOperation(value = "getting local DateTime", nickname = "getDateTime")
 	@GetMapping("now")
-	public ResponseEntity<LocalDateTime> getNow() {
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = LocalDateTime.class) })
+	ResponseEntity<LocalDateTime> getNow() {
 		return new ResponseEntity<>(LocalDateTime.now(), HttpStatus.OK);
 	}
 }
